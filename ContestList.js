f1c5567220efa7da8cca1366e85aa9e7860da230
gloriaLVela <37721059+gloriaLVela@users.noreply.github.com>
2019-01-22 13:49:25 -0600

    Add files via upload
    Looking up the contest on route

---
+++ ContestList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ContestPreview from './ContestPreview';
+
+const ContestList = ({ contests, onContestClick }) => {
+  return (
+        <div>
+        {contests.map(contest => 
+            <ContestPreview 
+            key={contest.id} 
+            onClick={onContestClick}
+            {...contest} />
+        )}
+        </div>
+    );
+};
+
+ContestList.propTypes = {
+  contests: React.PropTypes.array,
+  onContestClick: React.PropTypes.func.isRequired
+};
+
+export default ContestList;
